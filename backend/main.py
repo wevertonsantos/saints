5c48196abbee5e9df54d08b41f547af99ce0fb08
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-04-05 10:00:51 -0300

    configuração inicial API

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="Saints API",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app",host="0.0.0.0", port=8000,reload=True)
