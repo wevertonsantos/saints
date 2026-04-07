5de9bc237d8a57ab2159a1ac67c5829654eacb94
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-04-07 18:48:53 -0300

    tirando ponto e vírgula

--- frontend/js/logo.js
+++ frontend/js/logo.js
@@ -1,12 +1,12 @@
-const logo = document.querySelector(".logo");
+const logo = document.querySelector(".logo")
 
 export const showLogo = () => {
-    const title = document.createElement("h1");
-    const link = document.createElement("a");
-    const text = document.createTextNode("Saints");
-    logo.appendChild(title);
+    const title = document.createElement("h1")
+    const link = document.createElement("a")
+    const text = document.createTextNode("Saints")
+    logo.appendChild(title)
     title.classList.add("text")
-    title.appendChild(link);
-    link.appendChild(text);
-    link.setAttribute("href","home.html")
-}
+    title.appendChild(link)
+    link.appendChild(text)
+    link.setAttribute("href", "home.html")
+};
