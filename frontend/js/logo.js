d885a42c2a5a7add9b99cab741cc6ca8fe707585
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-04-08 18:40:19 -0300

    apenas espaço para melhor legibilidade

--- frontend/js/logo.js
+++ frontend/js/logo.js
@@ -4,6 +4,7 @@ export const showLogo = () => {
     const title = document.createElement("h1")
     const link = document.createElement("a")
     const text = document.createTextNode("Saints")
+    
     logo.appendChild(title)
     title.classList.add("text")
     title.appendChild(link)
