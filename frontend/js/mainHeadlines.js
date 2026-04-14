d07abfe47f5a5a5bb1cf743398a89dfafbfdd4bc
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-04-14 19:00:42 -0300

    adicionando 10 itens de lista com textos e títulos, alterando classes, e criando uma lista nova

--- frontend/js/mainHeadlines.js
+++ frontend/js/mainHeadlines.js
@@ -1,16 +1,19 @@
-const sectionMainHeadlines = document.querySelector(".main-headlines")
+const divMainHeadlines = document.querySelector(".main-headlines")
 
 export const mainHeadlines = () => {
     const list = document.createElement("ul")
+    const list2 = document.createElement("ul")
 
-    list.classList.add("items")
+    list.classList.add("images")
+    list2.classList.add("top-headlines")
 
-    items(list)
+    addItems(list,list2)
     
-    sectionMainHeadlines.appendChild(list)
+    divMainHeadlines.appendChild(list)
+    divMainHeadlines.appendChild(list2)
 }
 
-const items = (list) => {    
+const addItems = (list,list2) => {    
     for (let i = 0; i < 3; i++){
         const item = document.createElement("li")
         const link = document.createElement("a")
@@ -22,23 +25,37 @@ const items = (list) => {
         link.appendChild(img)
 
         if (i == 0) {
-            item.classList.add("item-1")
+            item.classList.add("img-1")
             img.classList.add("main-img")
             img.setAttribute("src", "../assets/handontablets.jpg")
         } else if (i == 1) {
-            item.classList.add("item-2")
+            item.classList.add("img-2")
             img.classList.add("second-img")
             img.setAttribute("src","../assets/handontablets.jpg")
         } else {
-            item.classList.add("item-3")
+            item.classList.add("img-3")
             img.classList.add("third-img")
             img.setAttribute("src","../assets/handontablets.jpg")
         }
     }
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 11; i++) {
         const item = document.createElement("li")
-        const link = document.createElement("a")
-        list.appendChild(item)
+        const h2 = document.createElement("h2")
+        const p = document.createElement("p")
+        const text = document.createTextNode("Principais Manchetes")
+        const headline = document.createTextNode("YouTube livestreams will now hold back ads during peak engagement to protect the vibe")
+
+        if (i == 0) {
+            list2.appendChild(h2)
+            h2.classList.add("text")
+            h2.appendChild(text)
+        } else {
+            list2.appendChild(item)
+            item.classList.add("headline")
+            item.classList.add(`${i}`)
+            item.appendChild(p)
+            p.appendChild(headline)
+        }
     }
 }
