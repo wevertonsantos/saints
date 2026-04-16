02bab017ad55c3b0b350965a555ddb5f40f81c43
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-04-16 19:27:11 -0300

    refazendo código, adicionando imgs e manchetes

--- frontend/js/mainHeadlines.js
+++ frontend/js/mainHeadlines.js
@@ -1,41 +1,40 @@
 const divMainHeadlines = document.querySelector(".main-headlines")
 
 export const mainHeadlines = () => {
-    const list = document.createElement("ul")
-    const list2 = document.createElement("ul")
+    const divMainImg = document.createElement("div")
+    const divTwoImgs = document.createElement("div")
+    const divTopHeadlines = document.createElement("div")
+    const div4 = document.createElement("div")
 
-    list.classList.add("images")
-    list2.classList.add("top-headlines")
+    divMainImg.classList.add("main-img")
+    divTwoImgs.classList.add("two-imgs")
+    divTopHeadlines.classList.add("top-headlines")
 
-    addItems(list,list2)
+    addItems(divMainImg,divTwoImgs,divTopHeadlines,)
     
-    divMainHeadlines.appendChild(list)
-    divMainHeadlines.appendChild(list2)
+    divMainHeadlines.appendChild(divMainImg)
+    divMainHeadlines.appendChild(divTwoImgs)
+    divMainHeadlines.appendChild(divTopHeadlines)
 }
 
-const addItems = (list,list2) => {    
-    for (let i = 0; i < 3; i++){
-        const item = document.createElement("li")
+const addItems = (divMainImg, divTwoImgs, divTopHeadlines) => {
+    const addImg = (div,attribute, imgClass, url) => {
         const link = document.createElement("a")
         const img = document.createElement("img")
-        
-        list.appendChild(item)
-        link.setAttribute("href","#")
-        item.appendChild(link)
+        link.setAttribute("href", attribute)
+        div.appendChild(link)
+        img.classList.add(imgClass)
         link.appendChild(img)
-
+        img.setAttribute("src", url)
+    }
+       
+    addImg(divMainImg, "#", "img-1", "../assets/handontablets.jpg")
+    
+    for (let i = 0; i < 2; i++){
         if (i == 0) {
-            item.classList.add("img-1")
-            img.classList.add("main-img")
-            img.setAttribute("src", "../assets/handontablets.jpg")
-        } else if (i == 1) {
-            item.classList.add("img-2")
-            img.classList.add("second-img")
-            img.setAttribute("src","../assets/handontablets.jpg")
+            addImg(divTwoImgs, "#", "img-2", "../assets/handontablets.jpg")
         } else {
-            item.classList.add("img-3")
-            img.classList.add("third-img")
-            img.setAttribute("src","../assets/handontablets.jpg")
+            addImg(divTwoImgs, "#", "img-3", "../assets/handontablets.jpg")
         }
     }
 
@@ -47,15 +46,16 @@ const addItems = (list,list2) => {
         const headline = document.createTextNode("YouTube livestreams will now hold back ads during peak engagement to protect the vibe")
 
         if (i == 0) {
-            list2.appendChild(h2)
+            divTopHeadlines.appendChild(h2)
             h2.classList.add("text")
             h2.appendChild(text)
         } else {
-            list2.appendChild(item)
+            divTopHeadlines.appendChild(item)
             item.classList.add("headline")
             item.classList.add(`${i}`)
             item.appendChild(p)
             p.appendChild(headline)
         }
     }
+
 }
