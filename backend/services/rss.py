9e1c3908e8257c9c9e48639d82b032fb853567c8
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-04-18 08:43:26 -0300

    trocando nome função e adicionando apenas o título na lista

--- backend/services/rss.py
+++ backend/services/rss.py
@@ -1,12 +1,12 @@
 import feedparser
 from core.config import settings
 
-def fetch_news():
+def fetch_titles_news():
     data = feedparser.parse(settings.RSS_URL)
 
     entries_list = []
 
     for entry in data.entries:
-        entries_list.append({"title":entry.title,"link":entry.link})
+        entries_list.append(entry.title)
 
     return entries_list
