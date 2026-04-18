f6e23a018b6693468c0b8be39abd86ad8b83bc38
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-04-18 19:51:52 -0300

    adicionando rota para pegar notícia pelo id

--- backend/routers/news.py
+++ backend/routers/news.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
+
 from sqlalchemy.orm import Session
 
 from db.database import get_db
+
 from models.news import News
+
 from schemas.news import NewsResponse
 
 router = APIRouter(
@@ -12,3 +15,10 @@ router = APIRouter(
 @router.get("/",response_model=list[NewsResponse])
 def get_news(db: Session = Depends(get_db)):
     return db.query(News).all()
+
+@router.get("/{news_id}", response_model=NewsResponse)
+def get_news_by_id(news_id: int, db: Session = Depends(get_db)):
+    news = db.query(News).filter(News.id == news_id).first()
+    if not news:
+        raise HTTPException(status_code=404, detail="News not found")
+    return news
