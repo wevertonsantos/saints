13d3c84c49afc930788394cb211733aefa70090d
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-04-18 17:36:05 -0300

    adicionando categoria na saida da extração dos entries

--- backend/services/scraper.py
+++ backend/services/scraper.py
@@ -1,13 +1,13 @@
 from newspaper import Article
-from services.rss import fetch_urls
+from services.rss import fetch_entries
 
 def extract_articles():
     articles = []
 
-    for url in fetch_urls():
-        article = Article(url)
+    for entry in fetch_entries():
+        article = Article(entry['url'])
         article.download()
         article.parse()
-        articles.append({'title':article.title,'content':article.text})
+        articles.append({'category':entry['category'],'title':article.title,'content':article.text})
 
     return articles
