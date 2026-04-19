ad975bc6ffb41dde03971ec8f3ad191fdf7d4f59
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-04-19 14:53:02 -0300

    verificando categorias que não apareça na lista de entries

--- backend/services/rss.py
+++ backend/services/rss.py
@@ -6,8 +6,8 @@ def fetch_entries():
 
     entries = []
 
-    for entry in data.entries:
-        if entry.tags[0]['term'] != settings.RSS_TAG:
+    for entry in data.entries[:2]:
+        if entry.tags[0]['term'] not in settings.RSS_TAGS:
             entries.append({'category':entry.tags[0]['term'],'url':entry.link})
 
     return entries
