1dfad9651de3ee0b35364768cb10da200929bf5a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-04-19 13:59:23 -0300

    adicionando job para salvar notícias no banco de dados a cada 1 hora

--- backend/main.py
+++ backend/main.py
@@ -7,16 +7,16 @@ from routers import news
 
 from core.config import settings
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from db.insert import save_news
+
 app = FastAPI(
     title="Saints API",
     version="0.1.0",
     docs_url="/docs",
     redoc_url="/redoc"
 )
-
-app.include_router(news.router,prefix=settings.API_PREFIX)
-
-create_tables()
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,6 +25,17 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+app.include_router(news.router,prefix=settings.API_PREFIX)
+
+create_tables()
+
+scheduler = BackgroundScheduler()
+
+scheduler.add_job(save_news, 'interval', hours=1)
+scheduler.start()
+
+save_news()
 
 if __name__ == "__main__":
     import uvicorn
